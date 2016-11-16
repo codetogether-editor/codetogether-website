da00ea5eab2f672c2499151002553a4eefec0145
tomik3690 <tomik031195@gmail.com>
2016-11-16 11:21:08 +0100

    'create file' dialog skeleton

--- components/actionButtons/controller.js
+++ components/actionButtons/controller.js
@@ -3,9 +3,14 @@ module.exports = function ($scope, $mdDialog) {
         template: require('./dialogs/share/template.html'),
         controller: require('./dialogs/share/controller.js')
     };
+
+    var createFileDialog = { 
+        template: require('./dialogs/createFile/template.html'),
+        controller: require('./dialogs/createFile/controller.js')
+    };
     
     $scope.createFile = () => {
-
+        $mdDialog.show(createFileDialog);
     };
 
     $scope.share = () => {
