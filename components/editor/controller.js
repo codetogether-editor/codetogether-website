16d0ec2e9b6e40134d2de8b3447a306fb826d0e7
WilhelmOlejnik <wilhelmolejnik@gmail.com>
2017-01-14 10:55:29 +0100

    now editor should distinguish insert/remove from keyborad and from code

--- components/editor/controller.js
+++ components/editor/controller.js
@@ -35,13 +35,13 @@ module.exports = async function ($scope, $rootScope, $state, Editor, $stateParam
     });
 
     // Local change
-    doc.on('change', (e) => {
-        var allowedActions = ['insert', 'remove'];
-        var userChange = editor.curOp && editor.curOp.command.name;
+    doc.on('change', (e) => { 
+        if (e != undefined && e.hasOwnProperty('isRemote') && e.isRemote == true)
+            return    
 
-        if (!userChange || allowedActions.indexOf(e.action) === -1) {
+        var allowedActions = ['insert', 'remove'];
+        if (allowedActions.indexOf(e.action) === -1)
             return;
-        }
 
         var startIndex = doc.positionToIndex(e.start, 0)
         var countChars = (lines) => (lines.reduce((a, b) => a + b.length + 1, 0) - 1)
@@ -66,18 +66,21 @@ module.exports = async function ($scope, $rootScope, $state, Editor, $stateParam
 
         if (command.type == 'add') {
             var position = doc.indexToPosition(command.index);
-            doc.insert(position, command.value);
-            if (prevCursorPos > command.index)
+            var isRemote = true
+            doc.insert(position, command.value, isRemote);
+            if(prevCursorPos > command.index)
                 newCursorPos = prevCursorPos + command.value.length
             editor.moveCursorToPosition(doc.indexToPosition(newCursorPos))
-
-        } else if (command.type == 'del') {
+            editor.clearSelection()
+            
+        } else if(command.type == 'del'){
             var fromPos = doc.indexToPosition(command.fromIndex)
             var toPos = doc.indexToPosition(command.toIndex)
-            doc.remove(new Range(fromPos.row, fromPos.column, toPos.row, toPos.column))
-            if (command.fromIndex < prevCursorPos && command.toIndex < prevCursorPos)
+            var isRemote = true
+            doc.remove(new Range(fromPos.row, fromPos.column, toPos.row, toPos.column), isRemote)
+            if(command.fromIndex < prevCursorPos && command.toIndex < prevCursorPos)
                 newCursorPos -= (command.toIndex - command.fromIndex)
-            else if (command.fromIndex < prevCursorPos && command.toIndex > prevCursorPos)
+            else if(command.fromIndex < prevCursorPos && command.toIndex > prevCursorPos)
                 newCursorPos -= (prevCursorPos - command.fromIndex)
             editor.moveCursorToPosition(doc.indexToPosition(newCursorPos))
         }
